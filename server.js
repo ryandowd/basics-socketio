7027bdb32fb06bd4fd37bb3fdf337a596c36e29d
Ryan Dowd <ryan.dowd84@gmail.com>
2020-04-17 11:49:15 +0100

    Add socket io

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
+
+io.on('connection', () => {
+    console.log('User connected via SocketIO');
+});
 
 http.listen(PORT, () => {
     console.log('Server Started!');
